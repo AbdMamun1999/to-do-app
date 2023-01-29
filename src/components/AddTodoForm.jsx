062f06b93b823f3c04f961866e1dbb8a59c1b18c
Abdullah Al Mamun <mamun12121999@gmail.com>
2023-01-29 17:21:59 +0600

    create add todo form

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 
 const AddTodoForm = () => {
+  const { currentTode, setcurrentTode } = useState("");
   return (
     <div>
-      
+      <div className="flex flex-row gap-x-2">
+        <input
+          className="flex-1 border-2 p-2 outline-none rounded text-primary focus:border-primary"
+          placeholder="Enter a todo name"
+          type="text"
+        />
+        {currentTode ? (
+          <button className="bg-primary rounded text-white border-2 px-4 py-2 border-primary">
+            <FaPlus />
+          </button>
+        ) : (
+          <button className="bg-primary rounded text-white border-2 px-4 py-2 border-primary">
+            <FaPlus />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import AddTodoForm from "./AddTodoForm";
 
 const Todo = () => {
   return (
-    <div>
-      
+    <div className="w-[400px] min-h-[550px] p-8 rounded-lg border-2 bg-white">
+      <div><h1 className='mb-2 text-primary font-semibold text-xl'>TO DO APP</h1>
+      </div>
+      <AddTodoForm/>
     </div>
   );
 };
