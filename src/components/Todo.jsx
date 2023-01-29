9416628ac0c12d715414f09e7699e665cfa5f6ca
Abdullah Al Mamun <mamun12121999@gmail.com>
2023-01-29 17:25:51 +0600

    design sorting button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,9 +4,19 @@ import AddTodoForm from "./AddTodoForm";
 const Todo = () => {
   return (
     <div className="w-[400px] min-h-[550px] p-8 rounded-lg border-2 bg-white">
-      <div><h1 className='mb-2 text-primary font-semibold text-xl'>TO DO APP</h1>
+      <div>
+        <h1 className="mb-2 text-primary font-semibold text-xl">TO DO APP</h1>
       </div>
-      <AddTodoForm/>
+      <AddTodoForm />
+
+      <div className="flex flex-row justify-between mt-3">
+        <button>All</button>
+        <button>Pending</button>
+        <button>Complete</button>
+        <button className="bg-primary px-2 py-1 rounded text-white">
+          Clear All
+        </button>
+      </div>
     </div>
   );
 };
